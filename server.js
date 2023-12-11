b113723e146136242f41fbb2ecc77c04090ee858
Your Name <you@example.com>
2023-12-10 21:13:49 -0500

    your message

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
